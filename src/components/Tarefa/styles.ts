dc5c487f7160d0f96bdc804b0607e4f38277076c
alanmdsan <alanmdsan@gmail.com>
2023-11-15 20:44:30 -0300

    setting enums

--- src/components/Tarefa/styles.ts
+++ src/components/Tarefa/styles.ts
@@ -1,4 +1,26 @@
 import styled from 'styled-components'
+import variaveis from '../../styles/variaveis'
+
+import * as enums from '../../utils/enums/Tarefa'
+
+type TagProps = {
+  prioridade?: enums.Prioridade
+  status?: enums.Status
+  parametro: 'status' | 'prioridade'
+}
+
+function retornaCorDeFundo(props: TagProps): string {
+  if (props.parametro === 'prioridade') {
+    if (props.prioridade === enums.Prioridade.URGENTE) return variaveis.vermelho
+    if (props.prioridade === enums.Prioridade.IMPORTANTE)
+      return variaveis.amarelo_2
+  } else {
+    if (props.status === enums.Status.PENDENTE) return variaveis.amarelo_1
+    if (props.status === enums.Status.CONCLUIDA) return variaveis.verde
+  }
+
+  return variaveis.azul_escuro
+}
 
 export const Card = styled.div`
   background-color: #fcfcfc;
@@ -14,12 +36,12 @@ export const Titulo = styled.h3`
   margin-bottom: 16px;
 `
 
-export const Tag = styled.span`
+export const Tag = styled.span<TagProps>`
   padding: 4px 8px;
   color: #fff;
   font-size: 10px;
   font-weight: bold;
-  background-color: #e1a32a;
+  background-color: ${(props) => retornaCorDeFundo(props)};
   border-radius: 8px;
   margin-right: 16px;
   display: inline-block;
@@ -51,7 +73,15 @@ export const Botao = styled.button`
   font-weight: bold;
   border: none;
   cursor: pointer;
-  background-color: #2f3640;
+  background-color: ${variaveis.azul_escuro};
   border-radius: 8px;
   margin-right: 8px;
+`
+
+export const BotaoSalvar = styled(Botao)`
+  background-color: ${variaveis.verde};
+`
+
+export const BotaoCancelarRemover = styled(Botao)`
+  background-color: ${variaveis.vermelho};
 `
---
+++ src/styles/variaveis.ts
@@ -0,0 +1,7 @@
+export default {
+  vermelho: '#c23616',
+  verde: '#44bd32',
+  amarelo_1: '#f0c11a',
+  amarelo_2: '#e1a32a',
+  azul_escuro: '#2f3640'
+}
