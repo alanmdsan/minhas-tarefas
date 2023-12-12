c6cfd896dd68cb1e28e96c1ecf7da70bd894dc3a
alanmdsan <alanmdsan@gmail.com>
2023-12-11 21:48:08 -0300

    fixing filter message

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux'
 import { RootReducer } from '../../store'
 
 import Tarefa from '../../components/Tarefa'
-import { Container } from './styles'
+import { Container, Resultado } from './styles'
 
 const ListaDeTarefas = () => {
   const { itens } = useSelector((state: RootReducer) => state.tarefas)
@@ -33,18 +33,31 @@ const ListaDeTarefas = () => {
     }
   }
 
+  const exibeResultadoFiltragem = (quantidade: number) => {
+    let mensagem = ''
+    const complementoTermo =
+      termo !== undefined && termo.length > 0 ? ` e "${termo}"` : ''
+
+    if (criterio === 'todas') {
+      mensagem = `${quantidade} tarefa${quantidade != 1 ? 's' : ''} encontrada${
+        quantidade != 1 ? 's' : ''
+      } como: todas${complementoTermo}`
+    } else {
+      mensagem = `${quantidade} tarefa${quantidade != 1 ? 's' : ''} encontrada${
+        quantidade != 1 ? 's' : ''
+      } como "${`${criterio} = ${valor}`}"${complementoTermo}`
+    }
+    return mensagem
+  }
+
+  const tarefas = filtraTarefas()
+  const mensagem = exibeResultadoFiltragem(tarefas.length)
+
   return (
     <Container>
-      <p>
-        2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;{termo}&ldquo;
-      </p>
+      <Resultado>{mensagem}</Resultado>
       <ul>
-        <li>{termo}</li>
-        <li>{criterio}</li>
-        <li>{valor}</li>
-      </ul>
-      <ul>
-        {filtraTarefas().map((t) => (
+        {tarefas.map((t) => (
           <li key={t.id}>
             <Tarefa
               id={t.id}
